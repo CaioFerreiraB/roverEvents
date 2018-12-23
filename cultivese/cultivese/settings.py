27bd0c40e4125fc1e92ce179efc1228a9e42ee9d
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2018-12-23 03:01:29 +0000

    make it better

--- cultivese/cultivese/settings.py
+++ cultivese/cultivese/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5pot4srj8^()x370+(@wx!m8#bs3-ckfd1jxmm^mzr2x!qs+qi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
