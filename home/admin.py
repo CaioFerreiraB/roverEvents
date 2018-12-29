ed361f55c040fc4eed70cfe72f06aaffd951a9a5
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2018-12-29 15:42:10 +0000

    'show_file_name'

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from home.models import Grupo, Participante
 
+
+admin.site.site_header = 'Cultive-se Admin'
+
+class ParticipanteAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'grupo')
+	search_fields = ('grupo__nome', 'nome')
+	
+
 # Register your models here.
 admin.site.register(Grupo)
-admin.site.register(Participante)
+admin.site.register(Participante, ParticipanteAdmin)
