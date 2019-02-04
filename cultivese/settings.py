ba54d755df5a973c1df4b5286f218df539c037b4
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2019-02-04 21:53:57 +0000

    'database_deploy'

--- cultivese/settings.py
+++ cultivese/settings.py
@@ -84,18 +84,15 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-DATABASES= {
-    'default': {
-        dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-}
-"""DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
+}
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
