65acac52021b07df0d8d9bf36adfb442a4f900a6
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2019-02-07 13:00:13 +0000

    'deploy'

--- cultivese/settings.py
+++ cultivese/settings.py
@@ -21,10 +21,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5pot4srj8^()x370+(@wx!m8#bs3-ckfd1jxmm^mzr2x!qs+qi'
+#SECRET_KEY = '5pot4srj8^()x370+(@wx!m8#bs3-ckfd1jxmm^mzr2x!qs+qi'
+#### ADD FOR PRODUCTION--------------------------------------------------
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#------------------------------------------------------------------------
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True   
+#DEBUG = True   
+#### ADD FOR PRODUCTION--------------------------------------------------
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+#------------------------------------------------------------------------
 
 ALLOWED_HOSTS = ['cultivese.herokuapp.com', '127.0.0.1']
 
